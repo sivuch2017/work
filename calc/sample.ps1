dc6bfcf1dcee6ee7493e0898a8b5dc4a5e26d317
SRA坂本 <sra00346@partner.nri.co.jp>
2018-06-05 19:15:44 +0900

    追加

--- calc/sample.ps1
+++ calc/sample.ps1
@@ -1,24 +1,43 @@
-#[void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-$url = "http://172.127.56.72:88/sra00346/4.html"
-$shell = New-Object -ComObject Shell.Application
+[void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.mshtml")
+
+$url = "http://172.127.56.72:88/sra00346/1.html"
+$user = "2817"
+$pass = "2817"
+
+$script:shell = New-Object -ComObject Shell.Application
 $ie = New-Object -ComObject InternetExplorer.Application
+$script:hwnd = $ie.HWND
+
+Function waitBusy {
+    Param([ref]$ie)
+    while ($ie.value.Document -isnot [mshtml.HTMLDocumentClass]) {
+        $ie.value = @($shell.Windows() | ? {$_.HWND -eq $hwnd})[-1]
+    }
+    while ($ie.value.busy -or $ie.value.readystate -ne 4) {
+        Start-Sleep -Milliseconds 100
+    }
+}
+
 $ie.Visible = $true
-$hwnd = $ie.HWND
-$ie.Navigate($url,4)
-while ($ie.Document -isnot [mshtml.HTMLDocumentClass]) {
-    $ie = $shell.Windows() | ? {$_.HWND -eq $hwnd}
+$ie.Navigate($url, 4)
+waitBusy([ref]$ie)
+
+@($ie.Document.getElementsByName("loginId"))[0].value = $user
+@($ie.Document.getElementsByName("passwd"))[0].value = $pass
+$ie.Document.getElementById("login_button").click()
+waitBusy([ref]$ie)
+
+# getElementsByClassNamemesoddo 
+# 漢字コードによってマッチングできないためAタグ3番目決め打ち
+@(@([System.__ComObject].InvokeMember("getElementsByClassName", [System.Reflection.BindingFlags]::InvokeMethod, $null, $ie.Document, "box"))[0].getElementsByTagName("a"))[2].click()
+waitBusy([ref]$ie)
+
+@([System.__ComObject].InvokeMember("getElementsByClassName", [System.Reflection.BindingFlags]::InvokeMethod, $null, $ie.Document, "box"))[2].getElementsByTagName("a") | ForEach-Object {
+    $_.click()
+    $ie2 = @($shell.Windows() | ? {$_.HWND -eq $hwnd})[-1]
+    waitBusy([ref]$ie2)
+    $ie2.Document.getElementsByName("workDate[]") | ForEach-Object {
+        Write-Host $_.Value $_.parentElement.childNodes.Item(4).innerText
+    }
 }
-while ($ie.busy -or $ie.readystate -ne 4) {
-    Start-Sleep -Milliseconds 100
-}
-$ie.Document.getElementsByName("workDate[]") | ForEach-Object {
-    # Write-Host $_.Value
-    # $_.parentElement.childNodes | foreach-object -begin {$bln = $true} -process {
-    #     if ($bln -and $_.TagName -eq "TD") {
-    #         if ($_.innerText -match "[0-9.]+\(([0-9.]+)\)") {
-    #             Write-Host $Matches[1]
-    #             $bln = $false
-    #         }
-    #     }
-    # }
-}
+
